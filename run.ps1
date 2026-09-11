16d22e06ad56b606de02dc42946f223482a1ea43
2alf <aalf@itu.dk>
2026-09-11 15:43:25 +0200

    Use default.nix only

--- run.ps1
+++ run.ps1
@@ -1,6 +1,11 @@
+$COMMIT = (git rev-parse HEAD).Trim()
+
 $BASE = "https://phoenix.whfd.itunderground.dk"
-$PACKAGE = "github:2alf/flx"
+$PACKAGE = "https://github.com/2alf/flx/archive/$COMMIT.tar.gz"
+
 $EncodedPackage = [System.Uri]::EscapeDataString($PACKAGE)
+
 $url = "$BASE/stream?package=$EncodedPackage"
+
 $response = Invoke-WebRequest -Uri $url
 $response.Content

